db8958f24880042d8eee22e404e51f862871b96a
Caique de Camargo <caique.de.camargo@hotmail.com>
2025-05-22 20:34:57 -0300

    implements the products service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      omitNull: true,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
